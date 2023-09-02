0eaf02debbcac46cac39741ea31d25ea16e8629c
wangdong18 <39229168+wangdong18@users.noreply.github.com>
2023-09-02 22:46:38 +0800

    27. 移除元素
    第二种逆向有点问题

---
+++ src/main/java/com/mortal/leetcode/Daily/RemoveElement/Solution.java
@@ -0,0 +1,33 @@
+package RemoveElement;
+
+/**
+ * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+ *
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{0,1,2,2,3,0,4,2};
+        int val = 2;
+        int i = removeElement(nums, val);
+        System.err.println(i);
+    }
+    public static int removeElement(int[] nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        //01403042
+        //01304042
+        for (int num : nums) {
+            System.err.print(num);
+        }
+        return index;
+    }
+}
