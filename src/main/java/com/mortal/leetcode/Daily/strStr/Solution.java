c04521fb96ff86290ed1d137f5ebb603b90d1449
wangdong18 <39229168+wangdong18@users.noreply.github.com>
2023-09-13 23:43:47 +0800

    28. 找出字符串中第一个匹配项的下标

---
+++ src/main/java/com/mortal/leetcode/Daily/strStr/Solution.java
@@ -0,0 +1,19 @@
+package strStr;
+
+public class Solution {
+
+    /**
+     * 给你两个字符串 haystack 和 needle ，
+     * 请你在 haystack 字符串中找出 needle 字符串的第一个匹配项的下标（下标从 0 开始）。
+     * 如果 needle 不是 haystack 的一部分，则返回  -1 。
+     * @param args
+     */
+    public static void main(String[] args) {
+       String haystack = "sadbutsad";
+       String needle = "sada";
+        System.err.println(strStr(haystack,needle));
+    }
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
